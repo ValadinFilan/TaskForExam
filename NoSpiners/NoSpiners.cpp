82faeaa8b26cb7fec2fae176756a25f3ebe0b12e
Valery Latonov <lvd_2002@mail.ru>
2020-10-19 10:58:58 +0300

    Добавьте файлы проекта.

---
+++ NoSpiners/NoSpiners.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int getInt(string massage) {
+    int Input;
+    cout << massage;
+    cin >> Input;
+    return Input;
+}
+int main()
+{
+    int a = getInt("a: ");
+    int b = getInt("a: ");
+    int result = 0;
+    for (int i = 0; i < a; i++)
+    {
+        for (int i = 0; i < b; i++)
+        {
+            result++;
+        }
+    }
+    cout << result;
+}
