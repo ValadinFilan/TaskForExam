877a913faad35a258a725c3e1fc7b14d2a991048
Valery Latonov <lvd_2002@mail.ru>
2020-10-19 18:39:20 +0300

    DeliteComments2

--- Balls/Balls.cpp
+++ Balls/Balls.cpp
@@ -1,11 +1,6 @@
 ﻿#include <iostream>
 #include <string>
 using namespace std;
-void outArray(int* a, int n) {
-	for (int i = 1; i <= n; i++)
-		cout << a[i];
-	cout << endl;
-}
 int factorial(int a) {
 	int result = 1;
 	for (int i = 1; i <= a; i++)
@@ -31,12 +26,10 @@ int* replace(int k1, int k2, int* a, int n) {
 int Moving(int n, int m, int* a) {
 	int result = 0;
 	for (int i = m; i <= n; i++)
-	{
 		if (a[i] == m)
 			result += factorial(n - m);
 		else
 			result += Moving(n, m + 1, replace(m, i, a, n));
-	}
 	return result;
 }
 int main()
@@ -45,6 +38,5 @@ int main()
 	int* a = new int[n+1];
 	for (int i = 1; i <= n; i++)
 		a[i] = i;
-	cout << endl;
 	cout << Moving(n, 1, a) << endl;
 }
